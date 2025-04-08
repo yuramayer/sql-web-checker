0ed15e4e03be5673a4fd7726fcaff2189c5ff9b0
yuramayer <votetopovorot2015@gmail.com>
2025-04-08 18:45:05 +0300

    feat: added working app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask App Web Page Module"""
 
-from flask import Flask, render_template, request
+from flask import Flask, Response, request, send_from_directory
 from processing import check_code
 
 app = Flask(__name__)
@@ -9,17 +9,39 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Main App Page"""
+
+    table_html = '<p style="color:red;"Введи SQL-код для обработки</p>'
+
     if request.method == 'POST':
 
-        text = request.form['text']
+        text_input = request.form.get('text_input', '')
 
-        df = check_code(text)
+        if text_input:
+            try:
+                df = check_code(text_input)
+                if df.empty:
+                    table_html = '<p style="color:green;">В \
+                        коде нет простых ошибок :)</p>'
+                else:
+                    table_html = df.to_html(
+                        index=False, classes='errors_table', escape=False)
+            except Exception as err:  # pylint: disable=broad-except
 
-        result_html = df.to_html(classes='table table-striped', index=False)
+                table_html = f'<p style="color:red;">\
+                    Ошибка обработки кода: {err}</p>'
 
-        return render_template('index.html', result=result_html)
+    with open('index.html', 'r', encoding='utf-8') as webpage:
+        html_content = webpage.read()
 
-    return render_template('index.html', result=None)
+    html_content = html_content.replace('<!-- TABLE -->', table_html)
+
+    return Response(html_content, mimetype='text/html')
+
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    """Add css to the flask web app"""
+    return send_from_directory('static', filename)
 
 
 if __name__ == '__main__':
